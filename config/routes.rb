1a4aa1650db00790e1ca0fa61a886d8245dfe43d
EC2 Default User <ec2-user@ip-172-31-7-196.ap-northeast-1.compute.internal>
2023-12-19 13:12:09 +0000

    [Create] 編集画面を作成し、更新機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
-  get 'books/edit'
+  get 'books/:id/edit' =>'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   get '/top' => 'homes#top'
 end
